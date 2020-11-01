90fc05ec6fb3eba8dc3bc2e37d324d1010c4c7a4
Your Name <coolerlair@gmail.com>
2020-10-31 23:59:00 -0300

    update make file, insert hardening...

---
+++ src/include/token_anti_csrf.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdint.h>
+
+// random string
+char salt[16];
+
+char * make_hash(unsigned char hash[]);
+int Base64encode_len(int len);
+int Base64encode(char *encoded, const char *string, int len);
+
+char *time_hour_minutes(int num);
+void *entropy_clock(void);
+char *rand_str(char *dst, int size);
+char *gen_anticsrf_token(int num);
+// check token 
+bool csrf_token_check(char *token_input);
+
+
---
+++ src/include/whitelist.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "mem_ops.h"
+
+//read lines of file
+bool whitelist_ip(char * addr);
